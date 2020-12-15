0081f10682ebe1253fcb221348d8444589a8e23d
Nicolas <novares.x@gmail.com>
2020-12-15 08:38:04 +0100

    Move to import to clean root folder

--- import/home-manager.nix
+++ import/home-manager.nix
@@ -4,10 +4,10 @@ with lib;
 
 let
   cfg = config.programs.shadow-client;
-  utilities = import ./utilities { inherit lib pkgs; };
+  utilities = import ../utilities { inherit lib pkgs; };
 
   # Declare the package with the appropriate configuration
-  shadow-package = pkgs.callPackage ./default.nix {
+  shadow-package = pkgs.callPackage ../default.nix {
     shadowChannel = cfg.channel;
     enableDiagnostics = cfg.enableDiagnostics;
     desktopLauncher = cfg.enableDesktopLauncher;
@@ -17,20 +17,20 @@ let
   drirc = utilities.files.drirc;
 in {
   # Import the configuration
-  imports = [ ./config.nix ./x-session ./systemd-session ];
+  imports = [ ../config.nix ];
 
   # By default, if you import this file, the Shadow app will be installed
   programs.shadow-client.enable = mkDefault true;
 
   # Enables
-  environment = mkIf cfg.enable {
+  home = mkIf cfg.enable {
     # Install Shadow wrapper
-    systemPackages = [ shadow-package ];
+    packages = with pkgs; [ shadow-package libva-utils libva ];
 
     # Add GPU fixes
-    etc.drirc.source = mkIf (cfg.enableGpuFix) drirc;
+    file.".drirc".source = mkIf (cfg.enableGpuFix) drirc;
 
     # Force VA Driver
-    variables.LIBVA_DRIVER_NAME = mkIf (cfg.forceDriver != "") [ cfg.forceDriver ];
+    sessionVariables.LIBVA_DRIVER_NAME = mkIf (cfg.forceDriver != "") [ cfg.forceDriver ];
   };
 }
